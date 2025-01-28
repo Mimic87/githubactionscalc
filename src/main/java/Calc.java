09f01573f33f9dd45903cee65452a1364e13bf33
JaimeHG <jaime.h.g@hotmail.com>
2025-01-28 16:34:15 +0100

    Update Calc.java

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -5,11 +5,11 @@ public class Calc {
     }
 
     public static int substract(int a, int b){
-        return a+b;
+        return a-b;
     }
 
     public static int multiply(int a, int b){
-        return a/b;
+        return a*b;
     }
 
     public static int div(int a, int b){
